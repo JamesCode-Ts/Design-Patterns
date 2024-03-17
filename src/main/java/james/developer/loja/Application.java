7ce4a5e7a3cbaad0c3e8dfbad55c34fbb169633a
Tiago Santana <63932833+JamesCode-Ts@users.noreply.github.com>
2024-03-17 20:10:50 -0300

    Update Application.java

--- src/main/java/james/developer/loja/Application.java
+++ src/main/java/james/developer/loja/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
- * Projeto Spring Boot gerado via Spring Initializr.
+ * 
  * Os seguintes módulos foram selecionados:
  * - Spring Data JPA
  * - Spring Web
